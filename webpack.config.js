c24e6c5cb9f403803dc3c050e9c95a875c85adbc
Tobias de Groen <tdgroen@zaibot.net>
2017-06-11 03:27:34 +0200

    wip

--- webpack.config.js
+++ webpack.config.js
@@ -6,100 +6,106 @@ const prodFn = (yes) => process.env.NODE_ENV === `production` ? yes : function()
 const prodIf = (yes, no) => process.env.NODE_ENV === `production` ? yes : no;
 
 const babelOptions = {
-  cacheDirectory: true,
-  presets: [
-    ["es2015", {
-      "modules": false
-    }]
-  ]
+    presets: [
+        ["es2015", {
+            "loose": true,
+            "modules": false
+        }]
+    ]
 };
 const cssLoaderOptions = {
-  modules: true,
-  importLoaders: 1,
-  localIdentName: prodIf(`[hash:base64:8]`, `[folder]_[name]__[local]--[hash:base64:5]`)
+    modules: true,
+    importLoaders: 1,
+    localIdentName: prodIf(`[hash:base64:8]`, `[folder]_[name]__[local]--[hash:base64:5]`)
 };
 
 module.exports = {
-  context: path.resolve(__dirname, `./src`),
-  cache: true,
-  devtool: `source-map`,
-  devServer: {
-    port: 3000,
-    inline: true,
-    contentBase: path.resolve(__dirname, `./static`)
-  },
-  entry: {
-    'index': [`babel-polyfill`, prodIf(`.`, `./demo`)]
-  },
-  module: {
-    loaders: [{
-        test: /\.tsx?$/,
+    context: path.resolve(__dirname, `./src`),
+    cache: true,
+    devtool: `source-map`,
+    devServer: {
+        port: 3000,
+        inline: true,
+        contentBase: path.resolve(__dirname, `./static`)
+    },
+    entry: {
+        'index': '.'
+    },
+    module: {
         loaders: [{
-          loader: `babel-loader`,
-          options: babelOptions
-        }, `ts-loader`]
-      },
-      {
-        test: /\.less$/,
-        loaders: [`classnames-loader`, `style-loader`, {
-          loader: `css-loader`,
-          options: cssLoaderOptions
-        }, `postcss-loader`, `less-loader`]
-      },
-      {
-        test: /\.css$/,
-        loaders: [`classnames-loader`, `style-loader`, {
-          loader: `css-loader`,
-          options: cssLoaderOptions
-        }, `postcss-loader`]
-      }
+                test: /\.tsx?$/,
+                loaders: [{
+                    loader: `babel-loader`,
+                    options: babelOptions
+                }, `ts-loader`]
+            },
+            {
+                test: /\.less$/,
+                loaders: [`classnames-loader`, `style-loader`, {
+                    loader: `css-loader`,
+                    options: cssLoaderOptions
+                }, `postcss-loader`, `less-loader`]
+            },
+            {
+                test: /\.css$/,
+                loaders: [`classnames-loader`, `style-loader`, {
+                    loader: `css-loader`,
+                    options: cssLoaderOptions
+                }, `postcss-loader`]
+            }
+        ]
+    },
+    node: {
+        Buffer: false
+    },
+    output: {
+        path: path.resolve(__dirname, `./dist`),
+        library: `material.js`,
+        filename: `[name].js`,
+        chunkFilename: `[id].js`,
+        libraryTarget: `commonjs2`,
+    },
+    externals: [
+        prodIf(NodeExternals({
+            whitelist: [/material-design-color-palette/]
+        })),
+    ].filter(Boolean),
+    resolve: {
+        extensions: [`.webpack.js`, `.tsx`, `.ts`, `.js`, `.json`],
+        alias: {}
+    },
+    plugins: [
+        prodFn(new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': `"production"`
+            }
+        })),
+        prodFn(new webpack.LoaderOptionsPlugin({
+            minimize: true,
+            debug: false
+        })),
+        prodFn(new webpack.optimize.AggressiveMergingPlugin()),
+        prodFn(new webpack.optimize.OccurrenceOrderPlugin()),
+        prodFn(new webpack.optimize.UglifyJsPlugin({
+            sourceMap: true,
+            compress: {
+                warnings: false,
+                screw_ie8: true,
+                conditionals: true,
+                unused: true,
+                comparisons: true,
+                sequences: true,
+                dead_code: true,
+                evaluate: true,
+                pure_getters: true,
+                if_return: true,
+                join_vars: true,
+                unsafe: false,
+                unsafe_comps: false,
+            },
+            output: {
+                comments: false,
+            }
+        }))
     ]
-  },
-  output: {
-    path: path.resolve(__dirname, `./dist`),
-    library: `material.js`,
-    filename: `[name].js`,
-    chunkFilename: `[id].js`,
-    libraryTarget: `commonjs2`,
-  },
-  externals: [
-    prodIf(NodeExternals({ whitelist: [/material-design-color-palette/] })),
-  ].filter(Boolean),
-  resolve: {
-    extensions: [`.webpack.js`, `.tsx`, `.ts`, `.js`, `.json`],
-    alias: {}
-  },
-  plugins: [
-    prodFn(new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': `"production"`
-      }
-    })),
-    prodFn(new webpack.LoaderOptionsPlugin({
-      minimize: true,
-      debug: false
-    })),
-    prodFn(new webpack.optimize.AggressiveMergingPlugin()),
-    prodFn(new webpack.optimize.OccurrenceOrderPlugin()),
-    prodFn(new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-        screw_ie8: true,
-        conditionals: true,
-        unused: true,
-        comparisons: true,
-        sequences: true,
-        dead_code: true,
-        evaluate: true,
-        pure_getters: true,
-        if_return: true,
-        join_vars: true,
-        unsafe: false,
-        unsafe_comps: false,
-      },
-      output: {
-        comments: false,
-      }
-    }))
-  ]
 };
--- webpack.demo.js
+++ webpack.demo.js
@@ -6,93 +6,94 @@ const prodFn = (yes) => process.env.NODE_ENV === `production` ? yes : function()
 const prodIf = (yes, no) => process.env.NODE_ENV === `production` ? yes : no;
 
 const babelOptions = {
-  cacheDirectory: true,
-  presets: [
-    ["es2015", {
-      "modules": false
-    }]
-  ]
+    cacheDirectory: true,
+    presets: [
+        ["es2015", {
+            "loose": true,
+            "modules": false
+        }]
+    ]
 };
 const cssLoaderOptions = {
-  modules: true,
-  importLoaders: 1,
-  localIdentName: `[folder]-[local]__[hash:base64:5]`
+    modules: true,
+    importLoaders: 1,
+    localIdentName: `[folder]-[local]__[hash:base64:5]`
 };
 
 module.exports = {
-  context: path.resolve(__dirname, `./src`),
-  cache: true,
-  devtool: `source-map`,
-  devServer: {
-    port: 3000,
-    inline: true,
-    contentBase: path.resolve(__dirname, `./static`)
-  },
-  entry: {
-    'index': [`babel-polyfill`, `./demo`]
-  },
-  module: {
-    loaders: [{
-        test: /\.tsx?$/,
+    context: path.resolve(__dirname, `./src`),
+    cache: true,
+    devtool: `source-map`,
+    devServer: {
+        port: 3000,
+        inline: true,
+        contentBase: path.resolve(__dirname, `./static`)
+    },
+    entry: {
+        'index': [`babel-polyfill`, `./demo`]
+    },
+    module: {
         loaders: [{
-          loader: `babel-loader`,
-          options: babelOptions
-        }, `ts-loader`]
-      },
-      {
-        test: /\.less$/,
-        loaders: [`classnames-loader`, `style-loader`, {
-          loader: `css-loader`,
-          options: cssLoaderOptions
-        }, `postcss-loader`, `less-loader`]
-      },
-      {
-        test: /\.css$/,
-        loaders: [`classnames-loader`, `style-loader`, {
-          loader: `css-loader`,
-          options: cssLoaderOptions
-        }, `postcss-loader`]
-      }
-    ],
-  },
-  output: {
-    path: path.resolve(__dirname, `./demo`),
-    filename: `[name].js`,
-  },
-  resolve: {
-    extensions: [`.webpack.js`, `.tsx`, `.ts`, `.js`, `.json`],
-  },
-  plugins: [
-    prodFn(new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': `"production"`
-      }
-    })),
-    prodFn(new webpack.LoaderOptionsPlugin({
-      minimize: true,
-      debug: false
-    })),
-    prodFn(new webpack.optimize.AggressiveMergingPlugin()),
-    prodFn(new webpack.optimize.OccurrenceOrderPlugin()),
-    prodFn(new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-        screw_ie8: true,
-        conditionals: true,
-        unused: true,
-        comparisons: true,
-        sequences: true,
-        dead_code: true,
-        evaluate: true,
-        pure_getters: true,
-        if_return: true,
-        join_vars: true,
-        unsafe: false,
-        unsafe_comps: false,
-      },
-      output: {
-        comments: false,
-      }
-    }))
-  ]
+                test: /\.tsx?$/,
+                loaders: [{
+                    loader: `babel-loader`,
+                    options: babelOptions
+                }, `ts-loader`]
+            },
+            {
+                test: /\.less$/,
+                loaders: [`classnames-loader`, `style-loader`, {
+                    loader: `css-loader`,
+                    options: cssLoaderOptions
+                }, `postcss-loader`, `less-loader`]
+            },
+            {
+                test: /\.css$/,
+                loaders: [`classnames-loader`, `style-loader`, {
+                    loader: `css-loader`,
+                    options: cssLoaderOptions
+                }, `postcss-loader`]
+            }
+        ],
+    },
+    output: {
+        path: path.resolve(__dirname, `./demo`),
+        filename: `[name].js`,
+    },
+    resolve: {
+        extensions: [`.webpack.js`, `.tsx`, `.ts`, `.js`, `.json`],
+    },
+    plugins: [
+        prodFn(new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': `"production"`
+            }
+        })),
+        prodFn(new webpack.LoaderOptionsPlugin({
+            minimize: true,
+            debug: false
+        })),
+        prodFn(new webpack.optimize.AggressiveMergingPlugin()),
+        prodFn(new webpack.optimize.OccurrenceOrderPlugin()),
+        prodFn(new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+                screw_ie8: true,
+                conditionals: true,
+                unused: true,
+                comparisons: true,
+                sequences: true,
+                dead_code: true,
+                evaluate: true,
+                pure_getters: true,
+                if_return: true,
+                join_vars: true,
+                unsafe: false,
+                unsafe_comps: false,
+            },
+            output: {
+                comments: false,
+            }
+        }))
+    ]
 };
