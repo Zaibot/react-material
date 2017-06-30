95a0b334db8973d16d1f2d1da453137e7c0e3d8d
Tobias de Groen <tdgroen@zaibot.net>
2017-06-30 16:17:09 +0200

    wip

--- src/demo/static.tsx
+++ src/demo/static.tsx
@@ -10,6 +10,7 @@ import Input from '../input';
 import { Material, Offset, Surface } from '../static';
 import { FocusTopRight } from '../surface/focus';
 import BorderRadius from '../utils/borderRadius';
+import Circle from '../utils/circle';
 import Position from '../utils/position';
 import cx from './style.less';
 
@@ -44,6 +45,19 @@ export class Static extends React.Component<any, any> {
                                     <span className={mdc(colors.text.black.darker)}>Content</span>
                                 </Surface>
                             </Offset>
+                        </Material>
+                    </DialogContents>
+                    <DialogContents>
+                        <Material width={100} height={100} borderRadius={BorderRadius.round(3)} elevation={3}>
+                            <Surface width={100} height={100} circle={new Circle(50, 50, Math.sqrt(50 * 50 + 50 * 50) * 1.0)} offset={Position.empty} className={mdc(colors.bg.red.n300)}>
+                                <span className={mdc(colors.text.black.darker)}>Content</span>
+                            </Surface>
+                            <Surface width={100} height={100} circle={new Circle(50, 50, Math.sqrt(50 * 50 + 50 * 50) * 0.8)} offset={Position.empty} className={mdc(colors.bg.indigo.n300)}>
+                                <span className={mdc(colors.text.white.darker)}>Content</span>
+                            </Surface>
+                            <Surface width={100} height={100} circle={new Circle(50, 50, Math.sqrt(50 * 50 + 50 * 50) * 0.6)} offset={Position.empty} className={mdc(colors.bg.grey.n50)} opacity={0.9}>
+                                <span className={mdc(colors.text.black.darker)}>Content</span>
+                            </Surface>
                         </Material>
                     </DialogContents>
                 </Dialog>
